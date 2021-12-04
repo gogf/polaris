fac5c4e8a7a39d542c0930f6e0f253c2a275997d
daguang <lianghuanan1997@gmail.com>
2021-12-04 19:00:07 +0800

    🎉 init rate-limit register

--- plugin/ratelimiter/ratelimit.go
+++ plugin/ratelimiter/ratelimit.go
@@ -5,3 +5,44 @@
 // You can obtain one at https://github.com/gogf/gf.
 
 package ratelimiter
+
+import (
+	"github.com/gogf/gf/v2/net/ghttp"
+	"github.com/polarismesh/polaris-go/api"
+	"log"
+	"time"
+)
+
+var (
+	limit, err = api.NewLimitAPI()
+	param      = api.NewQuotaRequest()
+)
+
+// Register .
+func Register(r *ghttp.Server, pattern ...string) {
+	if err != err {
+		log.Fatalf("fail to create consumerAPI, err is %v", err)
+	}
+	defer limit.Destroy()
+	if len(pattern) == 0 {
+		pattern = []string{"/*"}
+	}
+	param.SetNamespace("")
+	param.SetCluster("")
+	param.SetRetryCount(5)
+	param.SetTimeout(5 * time.Second)
+	r.BindHookHandlerByMap(pattern[0], map[string]ghttp.HandlerFunc{
+		ghttp.HookBeforeServe: func(r *ghttp.Request) {
+			getQuota, err := limit.GetQuota(param)
+			if err != nil {
+				log.Fatalf("fail to get Quota,err is %v", err)
+			}
+			defer getQuota.Done()
+			defer getQuota.Release()
+			result := getQuota.Get()
+			if result.Code != 0 {
+
+			}
+		},
+	})
+}
