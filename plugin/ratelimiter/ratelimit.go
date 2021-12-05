6f6c522d136e04565d5aefc2b3f8e04986b70b67
daguang <lianghuanan1997@gmail.com>
2021-12-05 18:46:27 +0800

    🎉 init rate-limit register

--- plugin/ratelimiter/ratelimit.go
+++ plugin/ratelimiter/ratelimit.go
@@ -7,14 +7,19 @@
 package ratelimiter
 
 import (
+	"fmt"
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/polarismesh/polaris-go/api"
 	"log"
+	"strings"
 )
 
 var (
 	limit, err = api.NewLimitAPI()
 	param      = api.NewQuotaRequest()
+	namespace  string
+	service    string
+	labelsStr  string
 )
 
 // Register .
@@ -22,13 +27,17 @@ func Register(r *ghttp.Server, limitExceededFunc func(r *ghttp.Request), pattern
 	if err != err {
 		log.Fatalf("fail to create consumerAPI, err is %v", err)
 	}
-	defer limit.Destroy()
 	if len(pattern) == 0 {
 		pattern = []string{"/*"}
 	}
-	param.SetLabels(map[string]string{})
-	param.SetNamespace("")
-	param.SetService("")
+
+	label, err := parseLabels(labelsStr)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	param.SetLabels(label)
+	param.SetNamespace(namespace)
+	param.SetService(service)
 
 	r.BindHookHandlerByMap(pattern[0], map[string]ghttp.HandlerFunc{
 		ghttp.HookBeforeServe: func(r *ghttp.Request) {
@@ -43,4 +52,21 @@ func Register(r *ghttp.Server, limitExceededFunc func(r *ghttp.Request), pattern
 			limitExceededFunc(r)
 		},
 	})
+}
+
+//解析标签列表
+func parseLabels(labelsStr string) (map[string]string, error) {
+	strLabels := strings.Split(labelsStr, ",")
+	labels := make(map[string]string, len(strLabels))
+	for _, strLabel := range strLabels {
+		if len(strLabel) < 1 {
+			continue
+		}
+		labelKv := strings.Split(strLabel, ":")
+		if len(labelKv) != 2 {
+			return nil, fmt.Errorf("invalid kv pair str %s", strLabel)
+		}
+		labels[labelKv[0]] = labelKv[1]
+	}
+	return labels, nil
 }
